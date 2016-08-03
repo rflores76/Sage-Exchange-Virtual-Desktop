05f7c77f1956efddb6cda4f095ceb32d0cb4394a
Majid <majid.razvi@sage.com>
2016-08-03 22:13:01 +0000

    interim commit

--- php/sale.php
+++ php/sale.php
@@ -9,7 +9,7 @@
     // Standard MIT License.
     // ===============================
 
-    require('envelope.php');
+    require('shared.php');
     
     // you (or your client's) merchant credentials.
     // grab a test account from us for development!
@@ -26,26 +26,57 @@
 
     // and then piecing together our XML request
     $xmlRequest = "<?xml version=\"1.0\" encoding=\"utf-16\"?>
-        <Request_v1 xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">
-          <Application>
+    <Request_v1 xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">
+        <Application>
             <ApplicationID>DEMO</ApplicationID>
             <LanguageID>EN</LanguageID>
-          </Application>
-          <Payments>
+        </Application>
+        <Payments>
             <PaymentType>
-              <Merchant>
+            <Merchant>
                 <MerchantID>$merchant_id</MerchantID>
                 <MerchantKey>$merchant_key</MerchantKey>
-              </Merchant>
-              <TransactionBase>
+            </Merchant>
+            <TransactionBase>
                 <TransactionID>$transaction_id</TransactionID>
                 <TransactionType>$transaction_type</TransactionType>
                 <Reference1>$order_number</Reference1>
                 <Amount>$amount</Amount>
-              </TransactionBase>
+            </TransactionBase>
+            <Customer>
+                <Name>
+                    <FirstName>John</FirstName>
+                    <MI></MI>
+                    <LastName>Smith</LastName>
+                </Name>
+                <Address>
+                    <AddressLine1>123 Address St</AddressLine1>
+                    <AddressLine2>Apt Z</AddressLine2>
+                    <City>Cityville</City>
+                    <State>VA</State>
+                    <ZipCode>12345</ZipCode>
+                    <Country>USA</Country>
+                    <EmailAddress>john.smith@example.com</EmailAddress>
+                    <Telephone>999-555-1234</Telephone>
+                    <Fax>999-555-4321</Fax>
+                </Address>
+            </Customer>
             </PaymentType>
-          </Payments>
-        </Request_v1>";
+        </Payments>
+    </Request_v1>";
     
 
+    // these methods are defined in shared.php
+    $tokenizedRequest = getEnvelope($xmlRequest);
+    $redirectUrl = getRedirectUrl();
+
 ?>
+
+<html>
+    <form method="POST" action="https://www.sageexchange.com/sevd/frmPayment.aspx">
+        <input type="hidden" name="request" value="<?php echo htmlspecialchars($tokenizedRequest); ?>" />
+        <input type="hidden" name="redirect_url" value="<?php echo "https://$redirectUrl" ?>" />
+        <input type="hidden" name="consumer_initiated" value="true" />
+        <input type="submit" value="Submit Payment" />
+    </form>
+</html>
